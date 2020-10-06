7b129d3fc395e106b54f88fe4f80f0c8563800b2
Muhammad Aliyu <nafsun11@gmail.com>
2020-10-06 10:15:13 +0100

    New Build

---
+++ build/precache-manifest.72549b70c07f1df118d13182345e924f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7eba1c68b118b312f4561ed5877f33bd",
+    "url": "./index.html"
+  },
+  {
+    "revision": "5ff37e052b32a5f7da74",
+    "url": "./static/css/main.45f6f4ef.chunk.css"
+  },
+  {
+    "revision": "95f6c5ea81280fd964c1",
+    "url": "./static/js/2.421c4b77.chunk.js"
+  },
+  {
+    "revision": "d6de89dad4acfc0a9b6c14c9a81e8d14",
+    "url": "./static/js/2.421c4b77.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5ff37e052b32a5f7da74",
+    "url": "./static/js/main.1393b933.chunk.js"
+  },
+  {
+    "revision": "be68e61d8d1efa4351e0",
+    "url": "./static/js/runtime-main.7833503f.js"
+  },
+  {
+    "revision": "66b2c6e6662fddc23ca03b8a82dce3fc",
+    "url": "./static/media/textail-logo.66b2c6e6.png"
+  }
+]);
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.d710e5c31b3ce0de925894893794f048.js"
+  "./precache-manifest.72549b70c07f1df118d13182345e924f.js"
 );
 
 self.addEventListener('message', (event) => {
